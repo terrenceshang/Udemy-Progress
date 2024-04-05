90186562d7322d9dd196ba4399c49056cc98d93d
Zenan Shang <terrenceshang2@gmail.com>
2024-04-05 15:31:03 +0200

    Investment done

--- React_Complete_Guide_2024/03-investment-calculator/src/App.jsx
+++ React_Complete_Guide_2024/03-investment-calculator/src/App.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 12000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header></Header>
+      <UserInput onChange={handleChange} input={userInput} />
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a duration greater than zero</p>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ React_Complete_Guide_2024/03-investment-calculator/src/components/UserInput.jsx
@@ -0,0 +1,51 @@
+export default function UserInput(props) {
+  const { onChange, userInput } = props;
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            onChange={(event) =>
+              onChange("initialInvestment", event.target.value)
+            }
+            value={UserInput.initialInvestment}
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            onChange={(event) =>
+              onChange("annualInvestment", event.target.value)
+            }
+            value={UserInput.initialInvestment}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            onChange={(event) => onChange("expectedReturn", event.target.value)}
+            value={UserInput.initialInvestment}
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            onChange={(event) => onChange("duration", event.target.value)}
+            value={UserInput.initialInvestment}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}

adc3ce4345c43eaf6a79c4c6cc6bac1ea2bdbf6c
Zenan Shang <terrenceshang2@gmail.com>
2024-04-09 11:24:17 +0200

    Tailwind css done

--- React_Complete_Guide_2024/04-reactart/src/components/AuthInputs.jsx
+++ React_Complete_Guide_2024/04-reactart/src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 import Button from "./Button";
 import Input from "./input";
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -31,17 +23,22 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6 ">
         <Input label="Email" invalid={emailNotValid} type="email"></Input>
         <Input
           label="Password"
           invalid={passwordNotValid}
           type="password"
         ></Input>
-      </ControlContainer>
-      <div className="actions">
-        <Button type="button">Create a new account</Button>
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
+          Create a new account
+        </button>
         <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>

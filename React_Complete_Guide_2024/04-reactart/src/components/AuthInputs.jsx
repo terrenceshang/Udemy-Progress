d46a2aec35f7321c845dd3240c81432ab7faf36e
Zenan Shang <terrenceshang2@gmail.com>
2024-04-09 10:59:40 +0200

    vanilla css done

--- React_Complete_Guide_2024/04-reactart/src/components/AuthInputs.jsx
+++ React_Complete_Guide_2024/04-reactart/src/components/AuthInputs.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { styled } from "styled-components";
+import Button from "./Button";
+import Input from "./input";
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -17,36 +27,22 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
-      </div>
+      <ControlContainer>
+        <Input label="Email" invalid={emailNotValid} type="email"></Input>
+        <Input
+          label="Password"
+          invalid={passwordNotValid}
+          type="password"
+        ></Input>
+      </ControlContainer>
       <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button type="button">Create a new account</Button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );

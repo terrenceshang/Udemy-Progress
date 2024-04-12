ec84d18627a9914f6da5ed8a52c29233c172c649
Zenan Shang <terrenceshang2@gmail.com>
2024-04-12 11:51:37 +0200

    project 8 done

--- React_Complete_Guide_2024/08-useEffect/src/components/DeleteConfirmation.jsx
+++ React_Complete_Guide_2024/08-useEffect/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -11,6 +36,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }

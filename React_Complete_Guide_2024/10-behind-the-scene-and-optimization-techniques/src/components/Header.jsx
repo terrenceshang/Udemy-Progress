faf2ea4d2efa8cb4701bbeebd811b6fab4e87b49
Zenan Shang <terrenceshang2@gmail.com>
2024-04-16 10:46:40 +0200

    project 10 done

--- React_Complete_Guide_2024/10-behind-the-scene-and-optimization-techniques/src/components/Header.jsx
+++ React_Complete_Guide_2024/10-behind-the-scene-and-optimization-techniques/src/components/Header.jsx
@@ -1,8 +1,8 @@
-import { log } from '../log.js';
-import logoImg from '../assets/logo.png';
+import { log } from "../log.js";
+import logoImg from "../assets/logo.png";
 
 export default function Header() {
-  log('<Header /> rendered', 1);
+  log("<Header /> rendered", 1);
 
   return (
     <header id="main-header">

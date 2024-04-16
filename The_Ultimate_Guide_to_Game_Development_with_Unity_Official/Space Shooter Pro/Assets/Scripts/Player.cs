0534132d4b2475d7410507d836a3dadcb3c228e7
Zenan Shang <terrenceshang2@gmail.com>
2024-04-16 16:22:32 +0200

    section 3 done

--- The_Ultimate_Guide_to_Game_Development_with_Unity_Official/Space Shooter Pro/Assets/Scripts/Player.cs
+++ The_Ultimate_Guide_to_Game_Development_with_Unity_Official/Space Shooter Pro/Assets/Scripts/Player.cs
@@ -13,26 +13,24 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(speed * Time.deltaTime * direction);
 
-        if (transform.position.y > 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
-        if (transform.position.x > 11)
+        if (transform.position.x > 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
-        else if (transform.position.x <= -11)
+        else if (transform.position.x <= -11.3f)
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
